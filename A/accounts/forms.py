86a871d9f960c735c46336b9c78fc03356fab512
Mobin <mobin.esmaeili00@gmail.com>
2025-04-25 23:46:20 +0330

    override clean method and setup confirm password in RegistrationForm

--- A/accounts/forms.py
+++ A/accounts/forms.py
@@ -5,7 +5,8 @@ from django.core.exceptions import ValidationError
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     def clean_username(self):
         username = self.cleaned_data['username']
@@ -20,3 +21,10 @@ class UserRegistrationForm(forms.Form):
         if user:
             raise ValidationError('Email already registered.')
         return email
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password1')
+        p2 = cd.get('password2')
+        if p1 and p2 and p1 != p2:
+            raise ValidationError('Passwords don\'t match')
