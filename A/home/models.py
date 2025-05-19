b7ec1c8adf459e00813555892b45995cd54194d8
Mobin <mobin.esmaeili00@gmail.com>
2025-05-19 21:22:49 +0330

    add post like

--- A/home/models.py
+++ A/home/models.py
@@ -1,3 +1,5 @@
+from pyexpat.errors import messages
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -20,6 +22,15 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('home:post_detail', args=(self.id, self.slug))
 
+    def likes_count(self):
+        return self.pvotes.count()
+
+    def user_can_like(self, user):
+        user_like = user.uvotes.filter(post=self)
+        if user_like.exists():
+            return True
+        return False
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomments')
@@ -31,3 +42,10 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.user} - {self.body[:30]} - {self.created.date()}'
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='uvotes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pvotes')
+
+    def __str__(self):
+        return f'{self.user} liked {self.post.slug}'
